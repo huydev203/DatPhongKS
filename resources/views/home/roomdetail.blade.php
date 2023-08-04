62b3515cebf955c1fcfb719b4925183fde76ee2c
huy <huyntdev203@gmail.com>
2023-08-05 06:27:52 +0700

    updatecode

--- resources/views/home/roomdetail.blade.php
+++ resources/views/home/roomdetail.blade.php
@@ -1,100 +1,154 @@
 @extends('home.layouts.layout')
 @section('content')
 
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="card mb-4">
+                    <img class="card-img-top" src="{{asset('storage/'.$rooms->image)}}" alt="">
+                    <div class="card-body">
+                        <h1>{{ $rooms->name }}</h1>
+                        <p class="card-text">{{ $rooms->description }}</p>
+                        <p class="card-text"><strong>Price:</strong> {{ $rooms->price }}</p>
+                        <a href="#" class="btn btn-primary">Book now</a>
+                    </div>
+                </div>
 
-<div class="container">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h3 class="mb-0">Facilities</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="{{asset('image/home/ic_bedroom.png')}}" alt="" class="img-fluid">
+                                <p class="mt-2 text-center"><strong>1 Bedroom</strong></p>
+                            </div>
+                            <div class="col-md-3">
+                                <img src="{{asset('image/home/ic_livingroom.png')}}" alt="" class="img-fluid">
+                                <p class="mt-2 text-center"><strong>1 Living Room</strong></p>
+                            </div>
+                            <div class="col-md-3">
+                                <img src="{{asset('image/home/ic_bathroom.png')}}" alt="" class="img-fluid">
+                                <p class="mt-2 text-center"><strong>1 Bathroom</strong></p>
+                            </div>
+                            <div class="col-md-3">
+                                <img src="{{asset('image/home/ic_diningroom 1.png')}}" alt="" class="img-fluid">
+                                <p class="mt-2 text-center"><strong>1 Dining Room</strong></p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="{{asset('image/home/ic_wifi 1.png')}}" alt="" class="img-fluid">
+                                <p class="mt-2 text-center"><strong>10 Mbps</strong></p>
+                            </div>
+                            <div class="col-md-3">
+                                <img src="{{asset('image/home/ic_kulkas.png')}}" alt="" class="img-fluid">
+                                <p class="mt-2 text-center"><strong>1 Refrigerator</strong></p>
+                            </div>
+                            <div class="col-md-3">
+                                <img src="{{asset('image/home/ic_tv.png')}}" alt="" class="img-fluid">
+                                <p class="mt-2 text-center"><strong>1 TV</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h3 class="mb-0">Reviews</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h5 class="mb-0">Excellent</h5>
+                                <p><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i> (11 reviews)</p>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="well well-sm">
+                                    <div class="text-right">
+                                        <a class="btn btn-success btn-green" href="#reviews-anchor" id="open-review-box">Leave a Review</a>
+                                    </div>
+                                    <div class="row" id="post-review-box" style="display:none;">
+                                        <div class="col-md-12">
+                                            <form accept-charset="UTF-8" action="#" method="post">
+                                                <input id="ratings-hidden" name="rating" type="hidden">
+                                                <textarea class="form-control animated" cols="50" id="new-review" name="comment" placeholder="Enter your review here..." rows="5"></textarea><br>
+                                                <div class="text-right">
+                                                    <div class="stars starrr" data-rating="0"></div>
+                                                    <a class="btn btn-danger btn-sm" href="#" id="close-review-box" style="display:none; margin-right: 10px;">
+                                                        <span class="glyphicon glyphicon-remove"></span>Cancel</a>
+                                                    <button class="btn btn-success btn-lg" type="submit">Save</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="review-block">
+                                    <div class="row">
+                                        <div class="col-sm-2">
+                                            <img src="{{asset('image/user.jpg')}}" class="img-rounded" alt="user-image">
+                                            <div class="review-block-name"><a href="#">nktpro</a></div>
+                                            <div class="review-block-date">January 29, 2016<br/>1 day ago</div>
+                                        </div>
+                                        <div class="col-sm-10">
+                                            <div class="review-block-rate">
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star-o"></i>
+                                            </div>
+                                            <div class="review-block-title">Good Hotel</div>
+                                            <div class="review-block-description">This is a good hotel, located in a great location with an awesome sea view. Rooms are clean and tidy with good level of amenities...</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="review-block">
+                                    <div class="row">
+                                        <div class="col-sm-2">
+                                            <img src="{{asset('image/user.jpg')}}" class="img-rounded" alt="user-image">
+                                            <div class="review-block-name"><a href="#">nktpro</a></div>
+                                            <div class="review-block-date">January 29, 2016<br/>1 day ago</div>
+                                        </div>
+                                        <div class="col-sm-10">
+                                            <div class="review-block-rate">
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star-o"></i>
+                                                <i class="fa fa-star-o"></i>
+                                            </div>
+                                            <div class="review-block-title">Great Food</div>
+                                            <div class="review-block-description">Their breakfast is really exceptional, could not forget the variety that they offered. From western to eastern cuisines, everything was very delicious...</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-    <div class="box-image">
-        <div>
-            <img style="width: 400px" id="mainImage" src="{{asset('storage/'.$rooms->image)}}" alt="">
-        </div>
-    </div>
-
-
-    <br />
-
-    <div class="room-detail">
-        <div class="description">
-            <h1>Mô tả chi tiết phòng</h1>
-            <p>{{$rooms->name}}</p>
-            <div class="facilities">
-                <h3>Tiện ích</h3>
-                <div class="box-facilities">
-                    <p>
-                        <img src="{{asset('image/home/ic_bedroom.png')}}" alt="" />
-
-                        1 phòng ngủ
-                    </p>
-                    <p>
-                        <img src="{{asset('image/home/ic_livingroom.png')}}" alt="" />
-
-                        1 phòng khách
-                    </p>
-                    <p>
-                        <img src="{{asset('image/home/ic_bathroom.png')}}" alt="" />
-
-                        1 phòng tắm
-                    </p>
-                    <p>
-                        <img src="{{asset('image/home/ic_diningroom 1.png')}}" alt="" />
-
-                        1 phòng ăn
-                    </p>
-                    <p>
-                        <img src="{{asset('image/home/ic_wifi 1.png')}}" alt="" />
-
-                        10 mbp/s
-                    </p>
-                    <p>
-                        <img src="{{asset('image/home/ic_kulkas.png')}}" alt="" />
-
-                        1 tủ lạnh
-                    </p>
-                    <p>
-                        <img src="{{asset('image/home/ic_tv.png')}}" alt="" />
-
-                        1 tivi
-                    </p>
+            <div class="col-lg-4">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h3 class="mb-0">Hotel Rules</h3>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-unstyled mb-0">
+                            <li>-No smoking, parties, or events</li>
+                            <li>-Check-in time is 8:00 AM</li>
+                            <li>-No pets</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-
     </div>
 
-    <div class="hotel-rules">
-        <h3>Nội quy chung</h3>
-        <ul>
-            <li>-Không hút thuốc, tiệc tùng, hoặc sự kiện</li>
-            <li>-Giờ nhận phòng từ 8:00</li>
-            <li>-Không nuôi động vật</li>
-        </ul>
-    </div>
-
-
-
-
-    <h3>Bình luận</h3>
-{{--    @if($comment->count() > 0)--}}
-{{--    @foreach($comment as $comments)--}}
-{{--    <div class="comment">--}}
-{{--        <p><strong>{{ $comments->user->name }}:</strong> {{ $comments->comment }}</p>--}}
-{{--        <span class="timestamp">{{ $comments->created_at->format('d/m/Y H:i') }}</span>--}}
-{{--    </div>--}}
-{{--    @endforeach--}}
-{{--    @else--}}
-{{--    <p>Chưa có bình luận nào cho phòng này</p>--}}
-{{--    @endif--}}
-</div>
-<h3 style="margin-top: 12px;">Phản hồi</h3>
-
-<form method="POST" action="{{ route('add_cmt') }}">
-    @csrf
-    <input type="hidden" name="room_id" value="{{ $rooms->id }}">
-    <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
-    <textarea name="comment" placeholder="Viết bình luận của bạn"></textarea>
-    <button type="submit">Gửi</button>
-</form>
-</div>
-</main>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,108 +1,109 @@
 @extends('home.layouts.layout')
 @section('content')
     <link rel="stylesheet" href="{{asset('css/home/home.css')}}" >
-<div class="swiper">
-    <div class="swiper-wrapper">
-        <div class="swiper-slide">
-            <img src="{{asset('image/home/ks.png')}}" alt="" />
-            <div class="text">
-                <h5>Welcome to</h5>
-                <h1>HS</h1>
-                <p>Chúng tôi rất vinh dự khi được phục vụ quý khách</p>
-                <a href="loaiphong.html"><button>Đặt phòng</button></a>
+    <div class="swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="{{asset('image/home/ks.png')}}" alt="" />
+                <div class="text">
+                    <h5>Welcome to</h5>
+                    <h1>HS</h1>
+                    <p>Chúng tôi rất vinh dự khi được phục vụ quý khách</p>
+                    <a href="loaiphong.html"><button class="btn btn-primary">Đặt phòng</button></a>
+                </div>
             </div>
+
+
         </div>
+        <div class="swiper-pagination"></div>
 
-
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-scrollbar"></div>
     </div>
-    <div class="swiper-pagination"></div>
 
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
-    <div class="swiper-scrollbar"></div>
-</div>
+    <div class="content1">
+        <form action="index.php?ctr=room-search" method="POST" class="row g-3">
 
-<div class="content1">
-    <form action="index.php?ctr=room-search" method="POST">
+            <div class="checkout col-md-3">
+                <label for=""><i class="fa-solid fa-calendar-days"></i>Ngày đặt phòng</label>
+                <input type="date" id="check_out_date" placeholder="Checkout" name="ngay_vao" data-min-date=today class="form-control">
+            </div>
+            <div class="checkout col-md-3">
+                <label for=""><i class="fa-solid fa-calendar-days"></i>Ngày trả phòng</label>
+                <input type="date" id="check_out_date" placeholder="Checkout" name="ngay_tra" data-min-date=today class="form-control">
+            </div>
+            <div style="width: 200px" >
+                <label> Loại Phòng</label>
+                <select  name="room_type" id="" class="form-select">
 
-        <div class="checkout">
-            <label for=""><i class="fa-solid fa-calendar-days"></i>Ngày đặt phòng</label>
-            <input type="date" id="check_out_date" placeholder="Checkout" name="ngay_vao" data-min-date=today>
-        </div>
-        <div class="checkout">
-            <label for=""><i class="fa-solid fa-calendar-days"></i>Ngày trả phòng</label>
-            <input type="date" id="check_out_date" placeholder="Checkout" name="ngay_tra" data-min-date=today>
-        </div>
-        <div style="width: 200px" >
-            <label>Loại Phòng</label>
-            <select  name="room_type" id="">
+                    @foreach($type as $types)
+                        <option value="{{$types->id}}" >{{$types->name}}</option>
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-md-3">
+                <input type="submit" name="search" value="Check" class="btn btn-primary">
+            </div>
+        </form>
 
-                @foreach($type as $types)
-                    <option value="{{$types->id}}" >{{$types->name}}</option>
-                    </option>
-                @endforeach
-            </select>
-        </div>
-        <input type="submit" name="search" value="Check">
-    </form>
+        <div class="most-rooms">
+            <h1>Phòng được quan tâm nhiều</h1>
+            <div class="box-rooms">
+                @foreach($rooms as $room)
+                    <div class="item-rooms">
 
-    <div class="most-rooms">
-        <h1>Phòng được quan tâm nhiều</h1>
-        <div class="box-rooms">
-               @foreach($rooms as $room)
-                <div class="item-rooms">
+                        <a href="{{route('detail',['id'=>$room->id])}}">
+                            <img src="{{asset('storage/'.$room->image)}}" alt="">
+                        </a>
+                        <div class="desc">
+                            <div class="top-desc">
+                                <h3>{{$room->name}}</h3>
+                                <p class="text-secondary">   Trạng thái : {{$room->status==0 ? "Trống" : "Đã Đặt"}}</p>
+                                </p>
+                            </div>
 
-                    <a href="{{route('detail',['id'=>$room->id])}}">
-                        <img style="text-align: center;  display: inline-block;
-  margin: 5px;" src="{{asset('storage/'.$room->image)}}" alt="">
-                    </a>
-                    <div class="desc">
-                        <div class="top-desc">
-                            <h3>{{$room->name}}</h3>
-                            <p class="text-secondary">   Trạng thái : {{$room->status==0 ? "Trống" : "Đã Đặt"}}</p>
-                            </p>
-                        </div>
+                            <p style=" letter-spacing: 1px;" class="main-desc">
+                                Loại phòng :{{$typer[$room->room_type]}}
+                            <p class="text-danger" >Giá {{$room->price}} VND / Ngày</p>
 
-                        <p style=" letter-spacing: 1px;" class="main-desc">
-                            Loại phòng :{{$typer[$room->room_type]}}
-                        <p class="text-danger" >Giá :{{$room->price}}/Ngày</p>
-
-                        <div class="buttom-desc">
-                            <a href="#">Đặt phòng<i class="fa-solid fa-caret-right"></i></a>
-                            <a href="{{route('detail',['id'=>$room->id])}}">Xem thêm<i class="fa-solid fa-caret-right"></i></a>
+                            <div class="buttom-desc">
+                                <a href="#" class="btn btn-primary">Đặt phòng<i class="fa-solid fa-caret-right"></i></a>
+                                <a href="{{route('detail',['id'=>$room->id])}}" class="btn btn-primary">Xem thêm<i class="fa-solid fa-caret-right"></i></a>
+                            </div>
                         </div>
                     </div>
+                @endforeach
+
+            </div>
+        </div>
+
+        <div class="block">
+            <h1>Dịch vụ của chúng tôi</h1>
+            <div class="about-service">
+                <div class="item">
+                    <i class="fas fa-user-shield"></i>
+                    <h3>AN TOÀN</h3>
+                    <p>An ninh được đảm bảo an toàn cho quý khách.</p>
                 </div>
-               @endforeach
-
-        </div>
-    </div>
-
-    <div class="block">
-        <h1>Dịch vụ của chúng tôi</h1>
-        <div class="about-service">
-            <div class="item">
-                <i class="fas fa-user-shield"></i>
-                <h3>AN TOÀN</h3>
-                <p>An ninh được đảm bảo an toàn cho quý khách.</p>
-            </div>
-            <div class="item">
-                <i class="fas fa-coffee"></i>
-                <h3>CÀ PHÊ</h3>
-                <p>Cà phê được phục vụ cho mỗi sớm mai thức dậy.</p>
-            </div>
-            <div class="item">
-                <i class="fa-solid fa-bell"></i>
-                <h3>BÁO THỨC</h3>
-                <p>Dịch vụ báo thức mỗi buổi sáng, đúng giờ.</p>
-            </div>
-            <div class="item">
-                <i class="fa-solid fa-wifi"></i>
-                <h3>WIFI</h3>
-                <p>Tốc độ mạng chất lượng, đường truyền ổn định.</p>
+                <div class="item">
+                    <i class="fas fa-coffee"></i>
+                    <h3>CÀ PHÊ</h3>
+                    <p>Cà phê được phục vụ cho mỗi sớm mai thức dậy.</p>
+                </div>
+                <div class="item">
+                    <i class="fa-solid fa-bell"></i>
+                    <h3>BÁO THỨC</h3>
+                    <p>Dịch vụ báo thức mỗi buổi sáng, đúng giờ.</p>
+                </div>
+                <div class="item">
+                    <i class="fa-solid fa-wifi"></i>
+                    <h3>WIFI</h3>
+                    <p>Tốc độ mạng chất lượng, đường truyền ổn định.</p>
+                </div>
             </div>
         </div>
-    </div>
 
-</div>
+    </div>
 @endsection
